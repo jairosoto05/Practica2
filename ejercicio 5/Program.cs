83ee1ad982793b9a92311c5bf5d5d8763ce58021
jairo soto <jairosoto05@gmail.com>
2020-09-08 23:46:52 -0400

    ejercicio 6 agregado

--- ejercicio 5/Program.cs
+++ ejercicio 5/Program.cs
@@ -50,6 +50,7 @@ namespace ejercicio_5
 
             bank();
 
+
         }
     }
 }
---
+++ ejercicio 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prestamo;
+            int numcuotas;
+            double interes;
+            double cuotas;
+
+            void calcularCuotas()
+            {
+                Console.WriteLine("ingrese el monto del prestamo");
+                prestamo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese la cantidad de cuotas");
+                numcuotas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese tasa de interes");
+                interes = Convert.ToDouble(Console.ReadLine())/1200;
+
+                double factor = Math.Pow(interes + 1, numcuotas);
+                cuotas = prestamo * (factor * interes)/(factor - 1);
+                Console.WriteLine(Math.Round(cuotas,2));
+            }
+
+            calcularCuotas();
+            Console.ReadKey();
+        }
+    }
+}
